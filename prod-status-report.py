f5798e9ca2bc302a714dc434498bb063782a4233
Jonathan Christison <jochrist@redhat.com>
2015-12-15 15:31:56 +0000

    Pull tasklist from todo.sh

--- prod-status-report.py
+++ prod-status-report.py
@@ -3,9 +3,13 @@ import argparse
 import json
 from jinja2 import Template, Environment, PackageLoader
 from datetime import date, timedelta
+import subprocess
+import re
+import pdb
 
 env = Environment(loader=PackageLoader('prod-status-report', 'templates'))
 template = env.get_template('prod-status-report.txt')
+
 #Logging
 import logging
 
@@ -21,9 +25,7 @@ def gen_subject():
 def work_areas():
     wa = dict()
     wa.update(name="Fuse")
-    wa.update(description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus varius dictum metus vel ornare. Aliquam ac rutrum turpis. Aenean hendrerit et nisi ac cursus. Sed a finibus nibh, tempus venenatis massa. Aliquam iaculis et urna vitae sodales. Phasellus cursus aliquam velit. Suspendisse sit amet lectus imperdiet, dapibus erat aliquam, eleifend enim. Sed convallis laoreet erat at vehicula. Curabitur euismod lacus lacinia rhoncus lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec tortor vitae urna hendrerit tempor." +
-
-     "Sed elementum purus risus, eget bibendum nisi ultrices at. In hac habitasse platea dictumst. Fusce condimentum vulputate nibh, aliquet sodales magna sollicitudin a. Fusce id rutrum magna, quis sollicitudin augue. Praesent cursus accumsan purus in condimentum. Nullam ac mollis ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec interdum ex sed nulla tincidunt cursus. Phasellus cursus quam at venenatis vehicula. Mauris hendrerit elementum mi, aliquet vestibulum mauris porta ut. Mauris elementum ante id luctus gravida. Quisque blandit vel velit interdum hendrerit. Integer ac nisl nibh")
+    wa.update(description="")
 
     return [wa]
 
@@ -42,12 +44,46 @@ def upcoming_events():
     ue = dict()
     return ue
 
+tasklist = subprocess.Popen(['todo.sh', 'projectview'], stdout=subprocess.PIPE).communicate()[0]
+#ansi_escape = re.compile(r'\x1b[^m]*m')
+#tasklist = ansi_escape.sub('',tasklist)
+def from_todo(projname=None):
+
+
+    #prj = re.compile('(---\s+)(?P<projtitle>.+\S)(\s+---)|(?P<taskid>^[0-9]+)\s((?P<priority>\([a-zA-Z]\)|)(?P<task>.+))')
+    #prj = re.compile('---\s+(?P<projtitle>.+\S)\s+---|(?P<taskid>[0-9]+)\s((?P<priority>\([A-Z]\)|)(?P<task>.+))')
+    prj = re.compile('---\s+(?P<projtitle>.+\S)\s+---')
+    if projname is None:
+        #tasklist = subprocess.Popen(['todo.sh', 'projectview'], stdout=subprocess.PIPE).communicate()[0]
+        for m in prj.finditer(tasklist):
+            yield m.group('projtitle')
+    else:
+        prj = re.compile('(?P<taskid>[0-9]+)\s((?P<priority>\([A-Z]\)|)(?P<task>.+))')
+        projtasklist = subprocess.Popen(['todo.sh', 'projectview', projname ], stdout=subprocess.PIPE).communicate()[0]
+        #Remove not in projects item
+        projtasklist = re.sub(r'(Not in projects.*)', r'', projtasklist, re.MULTILINE, re.DOTALL)
+        for m in prj.finditer(projtasklist):
+            if m.group('taskid') is not None:
+                yield m.group('taskid'), m.group('priority'), m.group('task')
+
+    #regex for --- PROJECT NAME --- and create list with children
+
+
 #Statistics about work done from meta information
 #No of coffees drank, number of line changes, number of incorrect commands etc
 #def statistics():
 
+
 def main():
     print "Subject: {}".format(gen_subject())
+    for proj in from_todo():
+        if proj is not None:
+            print '\'' + proj + '\''
+            for taskid, pri, task in from_todo(proj):
+                print taskid
+                print pri
+                print task
+
     print template.render(workareas=work_areas(),
             keyupdates=key_updates(),
             expectedevents=upcoming_events()
