00b5532966251fdd6d7b9abc44f0d6a4c07062a4
Jonathan Christison <jochrist@redhat.com>
2015-12-16 16:15:08 +0000

    Added todolist parsing and date aware history parsing

--- prod-status-report.py
+++ prod-status-report.py
@@ -2,16 +2,94 @@
 import argparse
 import json
 from jinja2 import Template, Environment, PackageLoader
-from datetime import date, timedelta
-import subprocess
+from datetime import datetime, date, timedelta
 import re
-import pdb
 
+#TODO functionality
+import subprocess
+import os
+import cStringIO
+#Logging
+import logging
+
+
+#from collections import defaultdict
+
+#debug
+import pdb
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+#Template stuff
 env = Environment(loader=PackageLoader('prod-status-report', 'templates'))
 template = env.get_template('prod-status-report.txt')
 
-#Logging
-import logging
+def tree():
+    return defaultdict(tree)
+
+class TodoList(object):
+    def __init__(self):
+        self.full_task_list = subprocess.Popen(['todo.sh', 'projectview'],
+                stdout=subprocess.PIPE).communicate()[0]
+        self.dic = self.as_dict()
+        self.history_text = cStringIO.StringIO
+        self.history_text = open(os.path.join(os.environ["TODO_DIR"],'done.txt'), 'rb').read()
+        self.history = [[d,t] for d, t in self._history()]
+
+    def _history(self, td=timedelta(days=7)):
+        task_regex = re.compile('x\s(?P<date>[-0-9]{10})\s(?P<task>.*)')
+        for m in task_regex.finditer(self.history_text):
+            dt = datetime.strptime(str(m.group('date')), "%Y-%m-%d")
+            days_diff = abs(((dt.date()) - date.today()))
+            if days_diff.days < td.days:
+                yield dt.date(), m.group('task')
+
+    def _projects(self):
+        prj = re.compile('---\s+(?P<projtitle>.+\S)\s+---')
+        for m in prj.finditer(self.full_task_list):
+            yield m.group('projtitle')
+
+    def _tasks(self, project):
+        prj = re.compile('(?P<taskid>[0-9]+)\s((?P<priority>\([A-Z]\)|)(?P<task>.+))')
+        projtasklist = subprocess.Popen(['todo.sh', 'projectview', project ], stdout=subprocess.PIPE).communicate()[0]
+        #Remove not in projects items
+        projtasklist = re.sub(r'(Not in projects.*)', r'', projtasklist, re.MULTILINE, re.DOTALL)
+        for m in prj.finditer(projtasklist):
+            if m.group('taskid') is not None:
+                yield m.group('taskid'), m.group('priority'), m.group('task')
+
+    def _contexts(self, task):
+        context = re.compile('@(?P<context>[-\w]+)')
+        for m in context.finditer(task):
+            yield m.group('context')
+
+    def _minutes_spent(self, task):
+        m = re.search(r'(min:([0-9]+))', task)
+        if m is not None:
+            return timedelta(minutes=int(m.group(2)))
+
+    def _strip(self, task):
+        return re.sub(r'\x1b[^m]*m|@[-\w]+|(min:([0-9]+))', '', task).strip()
+
+
+    def __iter__(self):
+        for p in self._projects():
+            for tid, pri, task in self._tasks(p):
+                yield tid, p, pri, self._strip(task), [c for c in self._contexts(task)], self._minutes_spent(task)
+
+    def as_dict(self):
+        ret = {}
+        for p in self._projects():
+            for tid, pri, task in self._tasks(p):
+                td = {}
+                self._minutes_spent(task)
+                td.update({'project':p})
+                td.update({'priority':pri})
+                td.update({'task':self._strip(task)})
+                td.update({'contexts':[c for c in self._contexts(task)]})
+                td.update({'timespent':self._minutes_spent(task)})
+                ret.update({tid:td})
+        return ret
 
 def gen_subject():
     subject = "Prod status "
@@ -22,12 +100,16 @@ def gen_subject():
     return subject
 
 #Maybe meta information from todo.sh/ip-config git commits
-def work_areas():
-    wa = dict()
-    wa.update(name="Fuse")
-    wa.update(description="")
+def work_areas(tdl):
+    wa = {}
 
-    return [wa]
+    for tid, p, pri, task, contexts, timespend in tdl:
+        if wa.has_key(p):
+            e = wa[p].get('events')
+            e.append(task)
+        else:
+            wa.update({p:{'events':[task]}})
+    return wa
 
 #Maybe a database/flatfile of events
 def significant_events():
@@ -44,30 +126,6 @@ def upcoming_events():
     ue = dict()
     return ue
 
-tasklist = subprocess.Popen(['todo.sh', 'projectview'], stdout=subprocess.PIPE).communicate()[0]
-#ansi_escape = re.compile(r'\x1b[^m]*m')
-#tasklist = ansi_escape.sub('',tasklist)
-def from_todo(projname=None):
-
-
-    #prj = re.compile('(---\s+)(?P<projtitle>.+\S)(\s+---)|(?P<taskid>^[0-9]+)\s((?P<priority>\([a-zA-Z]\)|)(?P<task>.+))')
-    #prj = re.compile('---\s+(?P<projtitle>.+\S)\s+---|(?P<taskid>[0-9]+)\s((?P<priority>\([A-Z]\)|)(?P<task>.+))')
-    prj = re.compile('---\s+(?P<projtitle>.+\S)\s+---')
-    if projname is None:
-        #tasklist = subprocess.Popen(['todo.sh', 'projectview'], stdout=subprocess.PIPE).communicate()[0]
-        for m in prj.finditer(tasklist):
-            yield m.group('projtitle')
-    else:
-        prj = re.compile('(?P<taskid>[0-9]+)\s((?P<priority>\([A-Z]\)|)(?P<task>.+))')
-        projtasklist = subprocess.Popen(['todo.sh', 'projectview', projname ], stdout=subprocess.PIPE).communicate()[0]
-        #Remove not in projects item
-        projtasklist = re.sub(r'(Not in projects.*)', r'', projtasklist, re.MULTILINE, re.DOTALL)
-        for m in prj.finditer(projtasklist):
-            if m.group('taskid') is not None:
-                yield m.group('taskid'), m.group('priority'), m.group('task')
-
-    #regex for --- PROJECT NAME --- and create list with children
-
 
 #Statistics about work done from meta information
 #No of coffees drank, number of line changes, number of incorrect commands etc
@@ -75,16 +133,19 @@ def from_todo(projname=None):
 
 
 def main():
+    tdl = TodoList()
+    pp.pprint(tdl.history)
     print "Subject: {}".format(gen_subject())
+    '''
     for proj in from_todo():
         if proj is not None:
             print '\'' + proj + '\''
             for taskid, pri, task in from_todo(proj):
-                print taskid
-                print pri
-                print task
-
-    print template.render(workareas=work_areas(),
+                print taskid + " " + pri + " " + task
+                #print pri
+                #print task
+    '''
+    print template.render(workareas=work_areas(tdl),
             keyupdates=key_updates(),
             expectedevents=upcoming_events()
             )
